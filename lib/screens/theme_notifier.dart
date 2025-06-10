0ffaf54bf5000bf885bdbf0288d1d3ad02e4d9d9
truongnguyenbao1 <137250130+truongnguyenbao1@users.noreply.github.com>
2025-06-10 14:28:48 +0700

    Update theme_notifier.dart

--- lib/screens/theme_notifier.dart
+++ lib/screens/theme_notifier.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class ThemeNotifier extends ChangeNotifier {
+  ThemeMode _mode = ThemeMode.light;
+  ThemeMode get mode => _mode;
+
+  void toggle(bool isDark) {
+    _mode = isDark ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
